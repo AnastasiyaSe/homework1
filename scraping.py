eefe8f747309bbc17c2f36744f7b895c3cf76999
AnastasiyaSe <79368124+AnastasiyaSe@users.noreply.github.com>
2021-10-14 20:28:04 +0300

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-DESIRED_HUBS = {'Научно-популярное' ,'C#'}
+DESIRED_HUBS = {'Научно-популярное ', 'C# *', 'Big Data '}
 
 response = requests.get('https://habr.com/ru/all/')
 if not response.ok:
@@ -10,10 +10,14 @@ if not response.ok:
 text = response.text
 soup = BeautifulSoup(text, features='html.parser')
 articles = soup.find_all('article', class_='tm-articles-list__item')
-print(len(articles))
 
 for article in articles:
-    hubs = {h.text for h in article.find_all('a',class_='tm-article-snippet__hubs-item-link')}
+    hubs = {h.text for h in article.find_all('a', class_='tm-article-snippet__hubs-item-link')}
+    time = {t.text for t in article.find('time')}
+    headline = {head.text for head in article.find('a', class_='tm-article-snippet__title-link')}
+
     if DESIRED_HUBS & hubs:
-        href = article.find('a', class_='tm-article-snippet__title_link').attrs.get('href')
-        print(href)
+        href = article.find('a', class_='tm-article-snippet__hubs-item-link').attrs.get('href')
+        print(headline)
+        print(time)
+        print(f'https://habr.com/ru{href}')
